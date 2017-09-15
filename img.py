16d721d47362030ee179fb3d64e2f08b8933d0b4
ppalbuquerque <albpedro897@hotmail.com>
2017-09-15 19:24:25 -0300

    add logaritmic function and power rating transformation

--- img.py
+++ img.py
@@ -3,27 +3,38 @@ import cv2
 
 def alargamento_constrate(image, limiar):
     height, width = image.shape
-    new_image = np.zeros((height, width))
     for h in range(0,height):
         for w in range(0,width):
             if image[h, w] < limiar:
-                new_image[h,w] = 0
+                image[h,w] = 0
             else:
-                new_image[h,w] = 1
-    cv2.imshow('new',new_image)
-    cv2.waitKey(0)
+                image[h,w] = 1
 
-def log_transformation(image, constant):
+def log_transformation(image, constant = 1):
     height, width = image.shape
-    new_image = np.zeros((height, width))
     for h in range(0,height):
         for w in range(0,width):
-            new_image[h,w] = constant * np.log10(image[h,w] + 1)
-    cv2.imshow('new',new_image)
-    cv2.waitKey(0)
+            image[h,w] = constant * np.log10(image[h,w] + 1)
+
+def powerrating_transformation(image, gama, constant = 1):
+    height, width = image.shape
+    for h in range(0,height):
+        for w in range(0,width):
+            image[h,w] = constant * (image[h,w] ** gama)
+
+def bits_plane(image, plane):
+    height, width = image.shape
+    new_image = np.zeros((height, width))
+    n = 8
+    for h in range(0,height):
+        for w in range(0,width):
+            bin_number = '{0:08b}'.format(image[h,w])
+            new_image[h,w] = int(bin_number[:n - plane],2)
+
 
 if __name__ == '__main__':
-    img = cv2.imread('photo.jpg', 0)
+    img = cv2.imread('ar_chaves.jpg', 0)
     cv2.imshow('image', img)
-    #alargamento_constrate(img, 50)
-    log_transformation(img, 1)
+    #alargamento_constrate(img, 100)
+    #log_transformation(img, 1)
+    powerrating_transformation(img, 1.0)
